cf94ee7d6d252e65a85ab5d372f81888e593185f
Giira <euan.a.marsh@gmail.com>
2024-11-30 16:12:12 +0000

    Part 2

--- day2.py
+++ day2.py
@@ -4,10 +4,15 @@ with open("day2.txt") as f:
 
 def main():
     total = 0
+    power_sum = 0
+
     for line in data:
         value = int(line.split(":")[0])
         sub_sets = line.split(":")[1].split(";")
         counted = True
+        red_h = 1
+        green_h = 1
+        blue_h = 1
 
         for sub_sub_set in sub_sets:
             cubes = sub_sub_set.split(",")
@@ -20,17 +25,28 @@ def main():
                     case "red":
                         if cube_tuple[0] > 12:
                             counted = False
+                        if cube_tuple[0] > red_h:
+                            red_h = cube_tuple[0]
                     case "green":
                         if cube_tuple[0] > 13:
                             counted = False
+                        if cube_tuple[0] > green_h:
+                            green_h = cube_tuple[0]
                     case "blue":
                         if cube_tuple[0] > 14:
                             counted = False
+                        if cube_tuple[0] > blue_h:
+                            blue_h = cube_tuple[0]
 
         if counted:
             total += value 
+        power = red_h * blue_h * green_h
+        power_sum += power
+
     print("======== Part 1 ========")
     print(total)
+    print("======== Part 2 ========")
+    print(power_sum)
 
 
 main()
