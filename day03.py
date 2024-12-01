3625b37b407d24e7af325bde8d2e4dd292dfc666
Giira <euan.a.marsh@gmail.com>
2024-12-01 22:02:04 +0000

    D3 Part 2

--- day03.py
+++ day03.py
@@ -2,6 +2,19 @@ with open("day03.txt") as f:
     data = [line.strip() for line in f.readlines()]
 
 
+def neighbours(coord):
+    neighbours = []
+    i = coord[0] - 1
+    for a in range(3):
+        j = coord[1] - 1
+        for b in range(3):
+            neighbours.append((i, j))
+            j += 1
+        i += 1
+    return neighbours
+        
+
+
 def main():
     positions_dic = {}
     symbols_pos = []
@@ -30,12 +43,12 @@ def main():
     for symbol in symbols_pos:
         x = symbol[0]-1
         for a in range(3):
-            # if x < 0 or x > i:
-            #     continue
+            if x < 0 or x > i:
+                continue
             y = symbol[1]-1
             for b in range(3):
-                # if y < 0 or y > j:
-                #     continue
+                if y < 0 or y > j:
+                    continue
                 if (x, y) not in search_coords:
                     search_coords.append((x, y))
                 y += 1
@@ -47,9 +60,34 @@ def main():
             if position in search_coords:
                 total += int(number)
                 break
+
+    asterisks = []
+    counts = {}
+    for symbol in symbols_pos:
+        if symbol[2] == "*":
+            asterisks.append((symbol[0], symbol[1]))
+    for asterisk in asterisks:
+        counts[asterisk] = []
+        
+    for coords, number in positions_dic.items():
+        for position in coords:
+            for asterisk in asterisks:
+                if position in neighbours(asterisk):
+                    if number not in counts[asterisk]:
+                        counts[asterisk].append(number)
+                    
+    total_2 = 0
+    for asterisk, count in counts.items():
+        if len(count) == 2:
+            total_2 += int(count[0]) * int(count[1])
+
+
     
     print("======== Part 1 ========")
     print(total)
+
+    print("======== Part 2 ========")
+    print(total_2)
 
 
 main()
