75cf8e8567912fd56879225b6a993de0681393fb
Giira <euan.a.marsh@gmail.com>
2024-12-01 18:27:23 +0000

    D3 Part 1

--- day03.py
+++ day03.py
@@ -11,7 +11,7 @@ def main():
         for j in range(len(data[0])):
             if data[i][j] == ".":
                 if number != "":
-                    positions_dic[number] = positions
+                    positions_dic[tuple(positions)] = number
                     number = ""
                     positions = []
             elif data[i][j].isnumeric():
@@ -19,13 +19,37 @@ def main():
                 positions.append((i, j))
             else:
                 if number != "":
-                    positions_dic[number] = positions
+                    positions_dic[tuple(positions)] = number
                     number = ""
                     positions = []
                 symbols_pos.append((i, j, data[i][j]))
+        if number != "":
+            positions_dic[tuple(positions)] = number
     
-    print(symbols_pos)
-    print(positions_dic)
+    search_coords = []
+    for symbol in symbols_pos:
+        x = symbol[0]-1
+        for a in range(3):
+            # if x < 0 or x > i:
+            #     continue
+            y = symbol[1]-1
+            for b in range(3):
+                # if y < 0 or y > j:
+                #     continue
+                if (x, y) not in search_coords:
+                    search_coords.append((x, y))
+                y += 1
+            x += 1
+    
+    total = 0
+    for coords, number in positions_dic.items():
+        for position in coords:
+            if position in search_coords:
+                total += int(number)
+                break
+    
+    print("======== Part 1 ========")
+    print(total)
 
 
 main()
